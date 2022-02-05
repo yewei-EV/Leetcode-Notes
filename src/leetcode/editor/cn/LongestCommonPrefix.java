33eb798d41082c2e7c75fa5b54732925d94db5fe
Yewei <milk0730@gmail.com>
2022-02-05 20:12:50 +0800

    leetcode 1725

---
+++ src/leetcode/editor/cn/LongestCommonPrefix.java
@@ -0,0 +1,64 @@
+/**
+  * 题目Id：14
+  * 题目：Longest Common Prefix
+  * 日期：2022-02-05 18:12:57
+*/
+//Write a function to find the longest common prefix string amongst an array of 
+//strings. 
+//
+// If there is no common prefix, return an empty string "". 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: strs = ["flower","flow","flight"]
+//Output: "fl"
+// 
+//
+// Example 2: 
+//
+// 
+//Input: strs = ["dog","racecar","car"]
+//Output: ""
+//Explanation: There is no common prefix among the input strings.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= strs.length <= 200 
+// 0 <= strs[i].length <= 200 
+// strs[i] consists of only lower-case English letters. 
+// 
+// Related Topics 字符串 👍 2007 👎 0
+
+package leetcode.editor.cn;
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        Solution solution = new LongestCommonPrefix().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if (strs[j].length() <= i || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
---
+++ src/leetcode/editor/cn/NumberOfRectanglesThatCanFormTheLargestSquare.java
@@ -0,0 +1,71 @@
+/**
+  * 题目Id：1725
+  * 题目：Number Of Rectangles That Can Form The Largest Square
+  * 日期：2022-02-05 18:13:00
+*/
+//You are given an array rectangles where rectangles[i] = [li, wi] represents 
+//the iᵗʰ rectangle of length li and width wi. 
+//
+// You can cut the iᵗʰ rectangle to form a square with a side length of k if 
+//both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut 
+//it to get a square with a side length of at most 4. 
+//
+// Let maxLen be the side length of the largest square you can obtain from any 
+//of the given rectangles. 
+//
+// Return the number of rectangles that can make a square with a side length of 
+//maxLen. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: rectangles = [[5,8],[3,9],[5,12],[16,5]]
+//Output: 3
+//Explanation: The largest squares you can get from each rectangle are of 
+//lengths [5,3,5,5].
+//The largest possible square is of length 5, and you can get it out of 3 
+//rectangles.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: rectangles = [[2,3],[3,7],[4,3],[3,7]]
+//Output: 3
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= rectangles.length <= 1000 
+// rectangles[i].length == 2 
+// 1 <= li, wi <= 10⁹ 
+// li != wi 
+// Related Topics 数组 👍 53 👎 0
+
+package leetcode.editor.cn;
+public class NumberOfRectanglesThatCanFormTheLargestSquare {
+    public static void main(String[] args) {
+        Solution solution = new NumberOfRectanglesThatCanFormTheLargestSquare().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countGoodRectangles(int[][] rectangles) {
+        int max = 0, ans = 0;
+        for (int[] r : rectangles) {
+            int cur = Math.min(r[0], r[1]);
+            if (cur == max) ans++;
+            else if (cur > max) {
+                max = cur;
+                ans = 1;
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
