5b7d7c4e51984b30fea53a28f219a9fc459f881f
Yewei <milk0730@gmail.com>
2022-02-07 23:45:37 +0800

    leetcode 15

---
+++ src/leetcode/editor/cn/ThreeSum.java
@@ -0,0 +1,72 @@
+/**
+  * 题目Id：15
+  * 题目：3Sum
+  * 日期：2022-02-06 22:33:40
+*/
+//Given an integer array nums, return all the triplets [nums[i], nums[j], nums[
+//k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0. 
+//
+// Notice that the solution set must not contain duplicate triplets. 
+//
+// 
+// Example 1: 
+// Input: nums = [-1,0,1,2,-1,-4]
+//Output: [[-1,-1,2],[-1,0,1]]
+// Example 2: 
+// Input: nums = []
+//Output: []
+// Example 3: 
+// Input: nums = [0]
+//Output: []
+// 
+// 
+// Constraints: 
+//
+// 
+// 0 <= nums.length <= 3000 
+// -10⁵ <= nums[i] <= 10⁵ 
+// 
+// Related Topics 数组 双指针 排序 👍 4268 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        Solution solution = new ThreeSum().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        if(nums == null || len < 3) return ans;
+        Arrays.sort(nums);
+        for (int i = 0; i < len ; i++) {
+            if(nums[i] > 0) break;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int L = i + 1;
+            int R = len - 1;
+            while(L < R){
+                int sum = nums[i] + nums[L] + nums[R];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
+                    while (L<R && nums[L] == nums[L+1]) L++; // 去重
+                    while (L<R && nums[R] == nums[R-1]) R--; // 去重
+                    L++;
+                    R--;
+                }
+                else if (sum < 0) L++;
+                else R--;
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
