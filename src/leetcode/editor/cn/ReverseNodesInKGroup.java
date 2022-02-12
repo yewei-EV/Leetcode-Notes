529a938d2016962d470d7621b2be4690b8026148
Yewei <milk0730@gmail.com>
2022-02-12 12:25:27 +0800

    leetcode 25

---
+++ src/leetcode/editor/cn/ReverseNodesInKGroup.java
@@ -0,0 +1,109 @@
+/**
+  * 题目Id：25
+  * 题目：Reverse Nodes in k-Group
+  * 日期：2022-02-10 00:22:56
+*/
+//Given the head of a linked list, reverse the nodes of the list k at a time, 
+//and return the modified list. 
+//
+// k is a positive integer and is less than or equal to the length of the 
+//linked list. If the number of nodes is not a multiple of k then left-out nodes, in 
+//the end, should remain as it is. 
+//
+// You may not alter the values in the list's nodes, only nodes themselves may 
+//be changed. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,3,4,5], k = 2
+//Output: [2,1,4,3,5]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,2,3,4,5], k = 3
+//Output: [3,2,1,4,5]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is n. 
+// 1 <= k <= n <= 5000 
+// 0 <= Node.val <= 1000 
+// 
+//
+// 
+// Follow-up: Can you solve the problem in O(1) extra memory space? 
+// Related Topics 递归 链表 👍 1463 👎 0
+
+package leetcode.editor.cn;
+public class ReverseNodesInKGroup {
+    public static void main(String[] args) {
+        Solution solution = new ReverseNodesInKGroup().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null) {
+            for(int i = 0; i < k && end != null; i++){
+                end = end.next;
+            }
+            if (end == null) {
+                break;
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = start;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode curNode = head;
+        ListNode nextNode = head;
+        ListNode preNode = null;
+        while (curNode != null) {
+            nextNode = curNode.next;
+            curNode.next = preNode;
+            preNode = curNode;
+            curNode = nextNode;
+        }
+        return preNode;
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
