4b34ab8bf466198f5af67a2446650a324abfd32d
Yewei <milk0730@gmail.com>
2022-02-13 11:46:43 +0800

    leetcode 221

---
+++ src/leetcode/editor/cn/MaximalSquare.java
@@ -0,0 +1,76 @@
+/**
+  * 题目Id：221
+  * 题目：Maximal Square
+  * 日期：2022-02-13 00:10:03
+*/
+//Given an m x n binary matrix filled with 0's and 1's, find the largest square 
+//containing only 1's and return its area. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1",
+//"1"],["1","0","0","1","0"]]
+//Output: 4
+// 
+//
+// Example 2: 
+//
+// 
+//Input: matrix = [["0","1"],["1","0"]]
+//Output: 1
+// 
+//
+// Example 3: 
+//
+// 
+//Input: matrix = [["0"]]
+//Output: 0
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 300 
+// matrix[i][j] is '0' or '1'. 
+// 
+// Related Topics 数组 动态规划 矩阵 👍 1024 👎 0
+
+package leetcode.editor.cn;
+public class MaximalSquare {
+    public static void main(String[] args) {
+        Solution solution = new MaximalSquare().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int max = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i < 1 || j < 1) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    }
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
