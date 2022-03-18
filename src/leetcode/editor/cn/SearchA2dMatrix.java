c040470a5c487d5c9570ab91eb2094c695bdab65
Yewei <milk0730@gmail.com>
2022-03-18 01:09:28 -0400

    leetcode 74

--- src/leetcode/editor/cn/SearchA2dMatrix.java
+++ src/leetcode/editor/cn/SearchA2dMatrix.java
@@ -46,39 +46,26 @@ public class SearchA2dMatrix {
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-        public boolean searchMatrix(int[][] matrix, int target) {
-            if (matrix.length < 1) return false;
-            int row = getRow(matrix, target);
-            return find(matrix[row], target);
-        }
-
-        public int getRow(int[][] matrix, int target) {
-            int top = 0, bottom = matrix.length - 1;
-            int col = matrix[0].length - 1;
-            while (top < bottom) {
-                int mid = (top + bottom) / 2;
-                if (matrix[mid][col] < target)
-                    top = mid + 1;
-                else
-                    bottom = mid;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length < 1) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int right = m * n  - 1;
+        int left = 0;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int i = mid / n;
+            int j = mid % n;
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
-            return top;
         }
-
-        public boolean find(int[] data, int target) {
-            int l = 0, r = data.length - 1;
-            while (l <= r) {
-                int mid = (l + r) / 2;
-                if (data[mid] == target)
-                    return true;
-                else if (data[mid] < target)
-                    l = mid + 1;
-                else
-                    r = mid - 1;
-            }
-            return false;
-        }
-
+        return false;
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
