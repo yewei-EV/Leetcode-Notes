ec8784aeb39b7d6bd92d7e0a18cd66d7e1f115c9
Yewei <milk0730@gmail.com>
2022-03-21 18:35:23 -0400

    leetcode 33

--- src/leetcode/editor/cn/SearchInRotatedSortedArray.java
+++ src/leetcode/editor/cn/SearchInRotatedSortedArray.java
@@ -49,25 +49,29 @@ public class SearchInRotatedSortedArray {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
-        while (l <= r) {
-            int m = l + (r - l)/2;
-            if (nums[m] == target) return m;
-            if (nums[m] < nums[nums.length - 1]) {
-                // m+1 to r is ordered
-                if (target <= nums[r] && target > nums[m]) {
-                    l = m + 1;
+        int n = nums.length;
+        int lo = 0, hi = n - 1;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if ((nums[mid] < nums[0]) == (target < nums[0])) {
+                // nums[mid] and target is on the same side
+                if (nums[mid] < target) {
+                    lo = mid + 1;
+                } else if (nums[mid] > target) {
+                    hi = mid - 1;
                 } else {
-                    r = m - 1;
+                    return mid;
                 }
+            } else if (target < nums[0]) {
+                // nums[mid] and target is on the different side
+                // target is on the right side
+                // shrink left boundary
+                lo = mid + 1;
             } else {
-                // l to m+1 is ordered
-                if (target >= nums[l] && target < nums[m]) {
-                    r = m - 1;
-                } else {
-                    l = m + 1;
-                }
+                // nums[mid] and target is on the different side
+                // target is on the left side
+                // shrink right boundary
+                hi = mid - 1;
             }
         }
         return -1;
