127c8fd510f8561d3f2448412cf01bb047c5420d
Yewei <milk0730@gmail.com>
2022-03-21 18:18:27 -0400

    leetcode 704

--- src/leetcode/editor/cn/BinarySearch.java
+++ src/leetcode/editor/cn/BinarySearch.java
@@ -46,15 +46,15 @@ public class BinarySearch {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
+        int n = nums.length - 1;
         int l = 0;
         int r = n;
-        while (l < r) {
+        while (l <= r) {
             int mid = l + (r-l)/2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
-                r = mid;
+                r = mid - 1;
             } else if (nums[mid] < target) {
                l = mid + 1;
             }
