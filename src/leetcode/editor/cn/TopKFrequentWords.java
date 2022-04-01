b2453ad992728c47a9a6896c402856777d1d154a
Yewei <milk0730@gmail.com>
2022-04-01 00:47:53 -0400

    leetcode 692

--- src/leetcode/editor/cn/TopKFrequentWords.java
+++ src/leetcode/editor/cn/TopKFrequentWords.java
@@ -62,7 +62,7 @@ class Solution {
         // Min Heap
         PriorityQueue<Map.Entry<String, Integer>> queue = new PriorityQueue<>((o1, o2) -> {
             if (Objects.equals(o1.getValue(), o2.getValue())) {
-                return o1.getKey().compareTo(o2.getKey());
+                return o2.getKey().compareTo(o1.getKey());
             } else {
                 return o1.getValue() - o2.getValue();
             }
