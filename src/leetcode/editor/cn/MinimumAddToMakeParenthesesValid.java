30c335380b009bb173ea16960d660db4df152854
Yewei <milk0730@gmail.com>
2022-04-04 00:18:24 -0400

    leetcode 921

--- src/leetcode/editor/cn/MinimumAddToMakeParenthesesValid.java
+++ src/leetcode/editor/cn/MinimumAddToMakeParenthesesValid.java
@@ -55,7 +55,15 @@ public class MinimumAddToMakeParenthesesValid {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minAddToMakeValid(String s) {
-
+        int ans = 0, bal = 0;
+        for (int i = 0; i < s.length(); i++) {
+            bal += s.charAt(i) == '(' ? 1 : -1;
+            if (bal == -1) {
+               ans++;
+               bal = 0;
+            }
+        }
+        return bal + ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
